947cc3190a237adb775288f61e681835bf565c3f
jenyas <jenyas@1touch.io>
2021-10-12 15:42:25 -0400

    init (hello world)

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
+
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+const root = {
+    message: () => 'Hello World!'
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(8080, () => console.log('Express GraphQL Server Now Running On localhost:8080/graphql'));
